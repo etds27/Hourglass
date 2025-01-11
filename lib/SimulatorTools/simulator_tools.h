d4dc362f1d07d6da722fcc43a130eaa61d0c273d
Ethan Schwarz <etds27@gmail.com>
2025-01-10 19:35:53 -0800

    Created simulator tools (millis)

--- lib/SimulatorTools/simulator_tools.h
+++ lib/SimulatorTools/simulator_tools.h
@@ -1,10 +1,6 @@
+#pragma once
 #ifdef SIMULATOR
-#include <chrono>
 
-unsigned long m_startTime = std::chrono::system_clock::now().time_since_epoch().count();
-
-unsigned long millis() {
-    return std::chrono::system_clock::now().time_since_epoch().count() - m_startTime;
-}
+unsigned long millis();
 
 #endif
