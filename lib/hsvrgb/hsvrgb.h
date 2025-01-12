cece127b1efe2a843c266e953c34a3069e47e555
Ethan Schwarz <etds27@gmail.com>
2025-01-11 18:22:54 -0800

    Removed original hsvrgb library

--- lib/hsvrgb/hsvrgb.h
+++
@@ -1,8 +0,0 @@
-
-#include <iostream>
-#include <cstdlib>
-#include <cmath>
-
-void RGBtoHSV(float &fR, float &fG, float fB, float &fH, float &fS, float &fV);
-void HSVtoRGB(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV)
-;
