6bdac7f9d6b46a70f5453de8561132f60bab1310
Ethan Schwarz <etds27@gmail.com>
2025-01-12 10:35:59 -0800

    testing color shift changes in main

--- src/main_sim.cpp
+++ src/main_sim.cpp
@@ -6,6 +6,7 @@
 #include "gl_ring_interface.h"
 #include "device_manager.h"
 #include "device_state.h"
+#include "color_converter.h"
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
@@ -48,40 +49,53 @@ void displayCallback()
   glLoadIdentity();           // Reset the model-view matrix
   uint8_t step = (millis() / 2000) % 3;
   uint8_t rotate = (millis() % 2000) / 2000.0 * 16;
-  std::cout << "Rotate: " << rotate << std::endl;
   uint32_t color = 0x000000;
+  uint32_t color2 = 0x000000;
 
     if (step == 0)
     {
       color = 0xFF0000;
+      color2 = 0x00FF00;
       logger.info("Red");
     }
     if (step == 1)
     {
       color = 0x00FF00;
+      color2 = 0x0000FF;
       logger.info("Green");
     }
     if (step == 2)
     {
       color = 0x0000FF;
+      color2 = 0xFF0000;
       logger.info("Blue");
     }
 
     for (int i = 0; i < 16; i++)
     {
       if (i <= 6) {
-        buffer[i] = m_ring->dimColor(color, (6 - i) / 6.0f * 255);
+        ColorTransform::DimColor* dimColor = new ColorTransform::DimColor((6 - i) / 6.0f * 255);
+        buffer[i] = dimColor->applyTransform(color);
+        delete dimColor;
       } else {
         buffer[i] = 0x000000;
       }
     }
-    uint32_t *fullBuffer = new uint32_t[16]{};
+    uint32_t* secondBuffer = new uint32_t[16]{};
+    m_ring->copyBuffer(buffer, secondBuffer, 16);
+    ColorTransform::SwapRedBlue* swapRedBlue = new ColorTransform::SwapRedBlue();
+    m_ring->transformBufferColor(secondBuffer, 16, swapRedBlue);
+    delete swapRedBlue;
+    m_ring->offsetBuffer(secondBuffer, 8);
+    m_ring->printBuffer(secondBuffer);
+    m_ring->overlayBuffer(buffer, secondBuffer, 16);
+        m_ring->printBuffer(buffer);
+
     m_ring->offsetBuffer(buffer, rotate);
-    m_ring->extendBuffer(buffer, fullBuffer, 8);
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     struct GameDebugData data{
-        .buffer = fullBuffer
+        .buffer = buffer
     };
     m_ring->updateGameDebugData(data);
 
@@ -92,7 +106,6 @@ void displayCallback()
   gl_tools::drawCircle(0.0, 0.0, 0.1, 0xAAAAAA);
 
   glutSwapBuffers();
-  delete[] fullBuffer;
 
 }
 
