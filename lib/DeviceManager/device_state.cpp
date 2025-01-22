c8ce1c0fa531fad2f2ec22a8346fdb1f45e8e3d2
Ethan Schwarz <etds27@gmail.com>
2025-01-21 21:26:11 -0800

    Added skip eligible device states

--- lib/DeviceManager/device_state.cpp
+++ lib/DeviceManager/device_state.cpp
@@ -1,25 +1,37 @@
 #include "device_state.h"
 
-namespace DeviceState {
-  const std::array<State, 2> ACTIVE_TURN_STATES = {
-      State::ActiveTurnEnforced,
-      State::ActiveTurnNotEnforced,
-  };
-
-  const std::array<State, 5> ACTIVE_GAME_STATES = {
-      State::ActiveTurnEnforced,
-      State::ActiveTurnNotEnforced,
-      State::AwaitingTurn,
-      State::Skipped,
-      State::Paused};
-
-bool isStateActiveTurn(State state)
+namespace DeviceState
 {
-    return std::find(ACTIVE_TURN_STATES.begin(), ACTIVE_TURN_STATES.end(), state) != ACTIVE_TURN_STATES.end();
-}
+    const std::array<State, 2> ACTIVE_TURN_STATES = {
+        State::ActiveTurnEnforced,
+        State::ActiveTurnNotEnforced,
+    };
 
-bool isStateActiveGame(State state)
-{
-    return std::find(ACTIVE_GAME_STATES.begin(), ACTIVE_GAME_STATES.end(), state) != ACTIVE_GAME_STATES.end();
-}
+    const std::array<State, 5> ACTIVE_GAME_STATES = {
+        State::ActiveTurnEnforced,
+        State::ActiveTurnNotEnforced,
+        State::AwaitingTurn,
+        State::Skipped,
+        State::Paused};
+
+    const std::array<State, 4> SKIP_ELIGIBLE_STATES = {
+        State::ActiveTurnEnforced,
+        State::ActiveTurnNotEnforced,
+        State::AwaitingTurn,
+        State::Skipped};
+
+    bool isStateActiveTurn(State state)
+    {
+        return std::find(ACTIVE_TURN_STATES.begin(), ACTIVE_TURN_STATES.end(), state) != ACTIVE_TURN_STATES.end();
+    }
+
+    bool isStateActiveGame(State state)
+    {
+        return std::find(ACTIVE_GAME_STATES.begin(), ACTIVE_GAME_STATES.end(), state) != ACTIVE_GAME_STATES.end();
+    }
+
+    bool isStateSkipEligible(State state)
+    {
+        return std::find(SKIP_ELIGIBLE_STATES.begin(), SKIP_ELIGIBLE_STATES.end(), state) != SKIP_ELIGIBLE_STATES.end();
+    }
 }
--- lib/DeviceManager/device_state.h
+++ lib/DeviceManager/device_state.h
@@ -7,7 +7,7 @@ namespace DeviceState
   enum class State
   {
     Off = 0,
-    AwaitingConnecion = 1,
+    AwaitingConnection = 1,
     AwaitingGameStart = 2,
     ActiveTurnEnforced = 3,
     ActiveTurnNotEnforced = 4,
@@ -21,7 +21,13 @@ namespace DeviceState
 
   extern const std::array<State, 5> ACTIVE_GAME_STATES;
 
+  /// @brief Collection of states where the Skip action is allowed
+  extern const std::array<State, 4> SKIP_ELIGIBLE_STATES;
+
   bool isStateActiveTurn(State state);
 
   bool isStateActiveGame(State state);
+
+  bool isStateSkipEligible(State state);
+
 }
