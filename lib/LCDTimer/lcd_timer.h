d38bddb98e14ee79820a12031af223e9a530a2d1
Ethan Schwarz <etds27@gmail.com>
2025-01-26 16:51:19 -0800

    Added clear to lcd timer

--- lib/LCDTimer/lcd_timer.h
+++ lib/LCDTimer/lcd_timer.h
@@ -5,13 +5,10 @@
 
 class LCDTimer : public HGDisplayInterface
 {
-
-
 public:
     LCDTimer(TFT_eSPI *tft);
     ~LCDTimer();
     // MARK: Light Modes
-    void updateLightModeActiveTurn();
     void updateLightModeActiveTurnTimer();
     void updateLightModeActiveTurnNoTimer();
     void updateLightModeSkipped();
@@ -21,15 +18,18 @@ public:
     void updateGameDebug();
     // Light ring mode for when the game is paused by the companion app
     void updateGamePaused();
+    void clear() override;
+
 
 protected:
     bool m_clearBeforeUpdate = false;
+    bool getClearBeforeUpdate() const override;
 
 private:
     uint32_t m_refreshRate = 1000;
     void begin();
-    void clear();
     void show();
+    void setTextSize();
 
     TFT_eSPI *m_tft;
 };
