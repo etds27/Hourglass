f26b04c0d11b3c150db68d744a707678e6f14c98
Ethan Schwarz <etds27@gmail.com>
2025-02-02 12:47:08 -0800

    Started adding game staate definiteions for each mode

--- lib/DeviceState/device_state.cpp
+++ lib/DeviceState/device_state.cpp
@@ -20,6 +20,32 @@ namespace DeviceState
         State::AwaitingTurn,
         State::Skipped};
 
+    const std::array<State, 7> SEQUENTIAL_TURN_ORDER_MODE_STATES = {
+        State::AwaitingConnection,
+        State::AwaitingGameStart,
+        State::ActiveTurnEnforced,
+        State::ActiveTurnNotEnforced,
+        State::AwaitingTurn,
+        State::Skipped,
+        State::Paused};
+
+    const std::array<State, 6> BUZZER_MODE_STATES = {
+        State::AwaitingConnection,
+        State::AwaitingGameStart,
+        State::ActiveTurnEnforced,
+        State::Skipped,
+        State::Paused};
+
+    // Off = 0,
+    // AwaitingConnection = 1,
+    // AwaitingGameStart = 2,
+    // ActiveTurnEnforced = 3,
+    // ActiveTurnNotEnforced = 4,
+    // AwaitingTurn = 5,
+    // Skipped = 6,
+    // Paused = 7,
+    // Debug = 8
+
     bool isStateActiveTurn(State state)
     {
         return std::find(ACTIVE_TURN_STATES.begin(), ACTIVE_TURN_STATES.end(), state) != ACTIVE_TURN_STATES.end();
